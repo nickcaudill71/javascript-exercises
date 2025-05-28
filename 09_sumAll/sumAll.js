9b57f3c32a89615afad3f6ded93f21736230b116
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-05-28 14:37:43 -0400

    Complete section 00_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((!Number.isInteger(a) || !Number.isInteger(b)) || (a < 0 || b < 0)) return 'ERROR';
 
+    let total = 0;
+    let [start, end] = a < b ? [a, b] : [b, a];
+
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+
+    return total
 };
 
 // Do not edit below this line
