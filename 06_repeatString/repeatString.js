ae296fce6bbca7b35c764d36e7c3953d555f5974
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-05-28 13:47:28 -0400

    Complete section 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, iter) {
+    if (iter < 0) return "ERROR";
 
+    let repeated = "";
+    for (let i = 0; i < iter; i++) {
+        repeated += word;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
