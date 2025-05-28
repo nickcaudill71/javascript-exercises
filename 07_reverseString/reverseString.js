3fd45ef1420344fc775e3eebd632ff0ba019696e
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-05-28 14:04:31 -0400

    Complete section 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const arr = word.split("");
+    const reverse = arr.reverse();
+    return reverse.join("");
 };
 
 // Do not edit below this line
