32f978b5f4e547b3be8cfd69ffa83c0d5a6e8ede
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-05-28 14:23:39 -0400

    Complete section 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    return arr.filter(elem => !valuesToRemove.includes(elem))
 };
 
 // Do not edit below this line
