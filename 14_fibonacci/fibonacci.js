3ab2f3a4cdaea712cbda2a32176f67d4590d1ea9
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-06-03 21:52:01 -0400

    Complete section 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let num = Number(value);
+    if (isNaN(num) || num < 0) return 'OOPS';
+    
+    let ans = 0;
 
+    if (num == 0) return 0;
+    if (num == 1 || num == 2) {
+        ans += 1;
+    }
+    else {
+        ans += fibonacci(num -1) + fibonacci(num - 2);
+    }
+    
+    return ans;
 };
 
 // Do not edit below this line
