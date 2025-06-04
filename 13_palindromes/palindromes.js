77ed47e1cc7ac900bf9dc39f542a897bf2469600
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-06-03 21:32:29 -0400

    Complete section 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const stripped = sentence.replaceAll(" ", "")
+                            .replaceAll(".", "")
+                            .replaceAll("!", "")
+                            .replaceAll(",", "")
+                            .toLowerCase();
+    let arr = Array.from(stripped).reverse();
+    reversedSentence = arr.join("")
 
+    let isPalDrome = stripped === reversedSentence ? true : false;
+    return isPalDrome;
 };
 
 // Do not edit below this line
