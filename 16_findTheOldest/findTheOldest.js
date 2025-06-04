ebfbd8d7af5bee220aad5f5a864d90ad6487fe51
Nicholas Caudill <108950705+nickcaudill71@users.noreply.github.com>
2025-06-03 22:14:32 -0400

    Complete section 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((prevOldest, currentPerson) => {
 
+        let oldAge;
+
+        if ("yearOfDeath" in prevOldest) {
+            oldAge = prevOldest.yearOfDeath - prevOldest.yearOfBirth;
+        }
+        else {
+            oldAge = new Date().getFullYear() - prevOldest.yearOfBirth;
+        }
+
+        const newAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        const newOldest = newAge > oldAge ? currentPerson : prevOldest;
+        return newOldest;
+    });
 };
 
 // Do not edit below this line
